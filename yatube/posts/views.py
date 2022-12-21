9e410c2fd464c398f9cce4d302f6c3a7b1fa1243
Владимир Андреяшин <vladimir.andreyashin@yandex.ru>
2022-12-21 11:29:54 +0300

    added an addition check in profile view

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -135,7 +135,10 @@ def follow_index(request):
 def profile_follow(request, username):
     user = request.user
     author = get_object_or_404(User, username=username)
-    if author == user:
+    if author == user and author.pk not in request.user.follower.all().values_list(
+        'author',
+        flat=True
+    ):
         return redirect('posts:follow_index')
     Follow.objects.create(user=user, author=author)
     return redirect('posts:follow_index')
