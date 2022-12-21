5a884f03b6b3eb4b032dc06c293fd4cd060ad164
Владимир Андреяшин <vladimir.andreyashin@yandex.ru>
2022-12-21 12:02:16 +0300

    changed profile_follow function

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -136,10 +136,11 @@ def profile_follow(request, username):
     user = request.user
     author = get_object_or_404(User, username=username)
     if author == user:
-        if author.pk not in request.user.follower.all().values_list(
-                            'author', flat=True):
             return redirect('posts:follow_index')
-    Follow.objects.create(user=user, author=author)
+    if author.pk not in (
+            request.user.follower.all().values_list('author', flat=True)
+            ):
+        Follow.objects.create(user=user, author=author)
     return redirect('posts:follow_index')
 
 
